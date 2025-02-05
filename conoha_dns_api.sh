0b0abf5210de0f63fdfb2352105136cdf6b2a8ac
takokun <mukoudakota8888@gmail.com>
2025-02-05 22:35:29 +0900

    - 未更新の関数をコメントアウト - get_conoha_tokenをAPIv3に対応
    TODO get_conoha_tokenのデバック

--- conoha_dns_api.sh
+++ conoha_dns_api.sh
@@ -10,48 +10,48 @@ source ${SCRIPT_PATH}/.env
 # FUNCTION #
 # -------- #
 get_conoha_token(){
-  curl -sS https://identity.${CNH_REGION}.conoha.io/v2.0/tokens \
+  curl -sS -i https://identity.${CNH_REGION}.conoha.io/v3/auth/tokens \
   -X POST \
   -H "Accept: application/json" \
-  -d '{ "auth": { "passwordCredentials": { "username": "'${CNH_USERNAME}'", "password": "'${CNH_PASSWORD}'" }, "tenantId": "'${CNH_TENANT_ID}'" } }' \
-  | jq -r ".access.token.id"
+  -d '{"auth":{"identity":{"methods":["password"],"password":{"user":{"name":"'${CNH_USERNAME}'","password":"'${CNH_PASSWORD}'"}}},"scope":{"project":{"id":"'${CNH_TENANT_ID}'"}}}}' \
+  | grep -i "x-subject-token" | awk '{print $2}'
 }
 
-get_conoha_domain_id(){
-  curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains \
-  -X GET \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
-  -H "X-Auth-Token: ${CNH_TOKEN}" \
-  | jq -r '.domains[] | select(.name == "'${CNH_DNS_DOMAIN_ROOT}'") | .id'
-}
+# get_conoha_domain_id(){
+#   curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains \
+#   -X GET \
+#   -H "Accept: application/json" \
+#   -H "Content-Type: application/json" \
+#   -H "X-Auth-Token: ${CNH_TOKEN}" \
+#   | jq -r '.domains[] | select(.name == "'${CNH_DNS_DOMAIN_ROOT}'") | .id'
+# }
 
-create_conoha_dns_record(){
-  curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records \
-  -X POST \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
-  -H "X-Auth-Token: ${CNH_TOKEN}" \
-  -d '{ "name": "'${CNH_DNS_NAME}'", "type": "'${CNH_DNS_TYPE}'", "data": "'${CNH_DNS_DATA}'", "ttl": 60 }'
-}
+# create_conoha_dns_record(){
+#   curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records \
+#   -X POST \
+#   -H "Accept: application/json" \
+#   -H "Content-Type: application/json" \
+#   -H "X-Auth-Token: ${CNH_TOKEN}" \
+#   -d '{ "name": "'${CNH_DNS_NAME}'", "type": "'${CNH_DNS_TYPE}'", "data": "'${CNH_DNS_DATA}'", "ttl": 60 }'
+# }
 
-get_conoha_dns_record_id(){
-  curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records \
-  -X GET \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
-  -H "X-Auth-Token: ${CNH_TOKEN}" \
-  | jq -r '.records[] | select(.name == "'${CNH_DNS_NAME}'" and .data == "'${CNH_DNS_DATA}'") | .id'
-}
+# get_conoha_dns_record_id(){
+#   curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records \
+#   -X GET \
+#   -H "Accept: application/json" \
+#   -H "Content-Type: application/json" \
+#   -H "X-Auth-Token: ${CNH_TOKEN}" \
+#   | jq -r '.records[] | select(.name == "'${CNH_DNS_NAME}'" and .data == "'${CNH_DNS_DATA}'") | .id'
+# }
 
-delete_conoha_dns_record(){
-  local delete_id=$1
-  curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records/${delete_id} \
-  -X DELETE \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
-  -H "X-Auth-Token: ${CNH_TOKEN}"
-}
+# delete_conoha_dns_record(){
+#   local delete_id=$1
+#   curl -sS https://dns-service.${CNH_REGION}.conoha.io/v1/domains/${CNH_DOMAIN_ID}/records/${delete_id} \
+#   -X DELETE \
+#   -H "Accept: application/json" \
+#   -H "Content-Type: application/json" \
+#   -H "X-Auth-Token: ${CNH_TOKEN}"
+# }
 
 # ----------- #
 # GET A TOKEN #
@@ -61,4 +61,4 @@ CNH_TOKEN=$(get_conoha_token)
 # ----------------- #
 # GET THE DOMAIN ID #
 # ----------------- #
-CNH_DOMAIN_ID=$(get_conoha_domain_id)
+# CNH_DOMAIN_ID=$(get_conoha_domain_id)
